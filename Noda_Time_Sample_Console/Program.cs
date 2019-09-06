deb26dfd38a66e8dc28931c30b8b39c5edd54795
James Kenny <james.m.kenny@gmail.com>
2019-09-06 13:09:44 +0100

    Add project files.

---
+++ Noda_Time_Sample_Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noda_Time_Sample_Console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Serversncode.com Noda Time demo starting");
+
+      // Get All timezones
+
+      var timeZones = TimeZoneInfo.GetSystemTimeZones();
+
+      foreach (var oTimeZone in timeZones)
+      {
+        Console.WriteLine("TimeZone: " + oTimeZone.DisplayName + " Id: " + oTimeZone.Id);
+      }
+
+
+
+      Console.WriteLine("Complete");
+    }
+  }
+}
