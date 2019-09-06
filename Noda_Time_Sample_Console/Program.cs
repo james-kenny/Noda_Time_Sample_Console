55bbe1a9a02aeb307cf4e7a00e860c559593e8b3
James Kenny <james.m.kenny@gmail.com>
2019-09-06 13:14:38 +0100

    Convert date from Time Zone to UTC

--- Noda_Time_Sample_Console/Program.cs
+++ Noda_Time_Sample_Console/Program.cs
@@ -14,6 +14,8 @@ namespace Noda_Time_Sample_Console
 
       // Get All timezones
 
+      Console.WriteLine("Get all Timezones");
+
       var timeZones = TimeZoneInfo.GetSystemTimeZones();
 
       foreach (var oTimeZone in timeZones)
@@ -21,10 +23,30 @@ namespace Noda_Time_Sample_Console
         Console.WriteLine("TimeZone: " + oTimeZone.DisplayName + " Id: " + oTimeZone.Id);
       }
 
+
+      Console.WriteLine("------------------------");
+      Console.WriteLine("Convert Date time to UTC");
+
+      // Set our fictional users time zone
       string easternZoneId = "Eastern Standard Time";
+
+      // The date they want to schedule the delivery.
+      var userScheduledTime = new DateTime(2018, 12, 03, 10, 46, 00);
+
+      // Load the NodaTimeZone information for the users timezone
+      TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById(easternZoneId);
+
+
+      // Convert the Date Time to UTC
+      var utcTime = TimeZoneInfo.ConvertTimeToUtc(userScheduledTime, easternZone);
+
+      Console.WriteLine("The date and time are " + utcTime + " UTC " + userScheduledTime + " " + easternZone.DisplayName);
+
+
 
 
       Console.WriteLine("Complete");
+      Console.ReadKey();
     }
   }
 }
